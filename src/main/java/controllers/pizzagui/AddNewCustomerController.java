3e607ab2775bd2203079675d04103d23afe98e3c
Nelso <albert.lobos13@gmail.com>
2022-04-18 16:57:48 -0400

    Finished most of Add new customer view. Set up the controller(methods have not been completed).

--- src/main/java/controllers/pizzagui/AddNewCustomerController.java
+++ src/main/java/controllers/pizzagui/AddNewCustomerController.java
@@ -3,10 +3,47 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import com.google.gson.*;
+import javafx.scene.control.TextField;
 
 public class AddNewCustomerController {
 
-    public void onHelloButtonClick(ActionEvent actionEvent) {
-        System.out.println("Worked");
+    @FXML
+    private TextField firstName;
+
+    @FXML
+    private TextField lastName;
+
+    @FXML
+    private TextField streetNum;
+
+    @FXML
+    private TextField streetName;
+
+    @FXML
+    private TextField stateName;
+
+    @FXML
+    private TextField cityName;
+
+    @FXML
+    private TextField zipCode;
+
+    @FXML
+    private TextField phoneNumber;
+
+    @FXML
+    private TextField creditNumber;
+
+    @FXML
+    private TextField ccvNumber;
+
+
+    public void addNewCustomer(ActionEvent actionEvent) {
+    }
+
+    public void goBackToStaffScreen(ActionEvent actionEvent) {
+    }
+
+    public void logOut(ActionEvent actionEvent) {
     }
 }
