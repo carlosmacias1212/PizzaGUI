47a6f3a7ed0a9194f7142b594ab32c625dd003e7
carlosmacias1212 <carlosmacias5545@gmail.com>
2022-04-26 05:03:16 -0400

    show orders

--- src/main/java/controllers/pizzagui/ShowOrdersController.java
+++ src/main/java/controllers/pizzagui/ShowOrdersController.java
@@ -1,27 +1,62 @@
 package controllers.pizzagui;
 
+import customer_info.Customer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import management.Order;
 import management.Staff;
+
+import static list.JsonController.customerList;
+import static list.JsonController.orderList;
 
 import java.io.IOException;
 
 public class ShowOrdersController {
 
+    public Label sceneTitle;
+    public VBox orderVbox;
     private Staff employee;
 
-    @FXML
-    public Label label;
+    public ScrollPane newScroll;
 
     public void displayOrders(){
+        orderVbox.setSpacing(25);
 
 
+        for (Customer c : customerList) {
+            String text = "";
+            text += "Phone Number: " + c.getPhoneNumber() + "\n";
+            text += "Delivery Information \n";
+            text += "    " + "Address" + "      " + c.getAddress().toString()+"\n\n";
+            text += "    Type:       Order Number: \n";
 
+            for (Order o : orderList) {
+                if (c.getPhoneNumber().equals(o.getCustomerPhoneNumber())) {
+                    if (o.getOrderType().equals(true)) {
+                        text += "    " + "pickup" + "        " + o.getOrderID()+"\n";
+                    } else {
+                        text += "    " + "delivery" + "      " + o.getOrderID()+"\n";
+                    }
+                }
+            }
+
+
+            Label newLabel = new Label();
+            newLabel.setStyle("-fx-font-family: Arial");
+            newLabel.setText(text);
+            orderVbox.getChildren().add(newLabel);
+           // newScroll.setContent(newLabel);
+
+        }
     }
 
     public void goBackToStaffView(ActionEvent actionEvent) throws IOException {
@@ -35,7 +70,7 @@ public class ShowOrdersController {
             staffViewController.setEmployee(getEmployee());
             staffViewController.displayName();
 
-            Stage window = (Stage) label.getScene().getWindow();
+            Stage window = (Stage) sceneTitle.getScene().getWindow();
 
             Scene scene = new Scene(root,900,600);
             window.setTitle("Manager View");
@@ -54,7 +89,7 @@ public class ShowOrdersController {
             staffViewController.setEmployee(getEmployee());
             staffViewController.displayName();
 
-            Stage window = (Stage) label.getScene().getWindow();
+            Stage window = (Stage) sceneTitle.getScene().getWindow();
 
             Scene scene = new Scene(root,900,600);
             window.setTitle("Staff View");
@@ -63,30 +98,28 @@ public class ShowOrdersController {
             window.show();
         }
     }
-
-    public void logOut(ActionEvent actionEvent) throws IOException{
-        changeView("Login-view.fxml");
-    }
-
-    public void changeView(String viewName) throws IOException {
-        Stage window = (Stage) label.getScene().getWindow();
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(viewName));
+    //This takes us back to the original Manager view
+    public void logOut(ActionEvent actionEvent) throws IOException {
+        Stage window = (Stage) sceneTitle.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(),900,600);
         window.setTitle("PieHackers Pizza Restaurant!");
         window.setScene(scene);
         window.setResizable(false);
         window.show();
     }
-
+    //This logs you out and send you back to the Login View
     public boolean isManager(){
         return employee.getEmployeeType().equals("Manager");
     }
-
+    //Checks to see if the employee is of type manager
     public Staff getEmployee() {
         return employee;
     }
+    //Getter for employee
 
     public void setEmployee(Staff employee) {
         this.employee = employee;
     }
+    //Setter for employee
 }
