07eb74925774352bef5e1f4da9842f7708539de7
rxrosario1117 <99367990+rxrosario1117@users.noreply.github.com>
2022-04-25 20:18:46 -0400

    Started on show order list button

---
+++ src/main/java/controllers/pizzagui/ShowOrdersController.java
@@ -0,0 +1,76 @@
+package controllers.pizzagui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import management.Staff;
+
+import java.io.IOException;
+
+public class ShowOrdersController {
+
+    private Staff employee;
+
+    @FXML
+    public Label label;
+
+    public void displayOrders(){
+
+    }
+
+    public void goBackToStaffView(ActionEvent actionEvent) throws IOException {
+
+        if(isManager()){
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Manager-View.fxml"));
+            Parent root = fxmlLoader.load();
+            StaffViewController staffViewController = fxmlLoader.getController();
+            fxmlLoader.setController(staffViewController);
+
+            staffViewController.setEmployee(getEmployee());
+            staffViewController.displayName();
+
+            Stage window = (Stage) label.getScene().getWindow();
+
+            Scene scene = new Scene(root,900,600);
+            window.setTitle("Manager View");
+            window.setScene(scene);
+            window.setResizable(false);
+            window.show();
+        }
+
+//        If it's a staff logged in, go back to staff view
+        else{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Staff-View.fxml"));
+            Parent root = fxmlLoader.load();
+            StaffViewController staffViewController = fxmlLoader.getController();
+            fxmlLoader.setController(staffViewController);
+
+            staffViewController.setEmployee(getEmployee());
+            staffViewController.displayName();
+
+            Stage window = (Stage) label.getScene().getWindow();
+
+            Scene scene = new Scene(root,900,600);
+            window.setTitle("Staff View");
+            window.setScene(scene);
+            window.setResizable(false);
+            window.show();
+        }
+    }
+
+    public boolean isManager(){
+        return employee.getEmployeeType().equals("Manager");
+    }
+
+    public Staff getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Staff employee) {
+        this.employee = employee;
+    }
+}
